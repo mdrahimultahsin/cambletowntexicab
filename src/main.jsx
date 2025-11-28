46d62d9d50a9f796d31f857809fab9ecd38469e8
Rahimul Tahsin <mdrahimultahsin@gmail.com>
2025-11-29 01:41:56 +0600

    added page title and changes code

--- src/main.jsx
+++ src/main.jsx
@@ -12,12 +12,7 @@ createRoot(document.getElementById("root")).render(
       libraries={["places"]}
       loadingElement={<Loading />}
     >
-
-
-          
-          <RouterProvider router={router} />
-    
-   
+      <RouterProvider router={router} />
     </LoadScript>
   </StrictMode>
 );
